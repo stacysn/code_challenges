7073b606845e3306ffbf03f218c9bb84ff853688
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-19 16:30:42 +0530

    Update service-lane.js
    using Math.min()

--- HackerRank-Javascript/service-lane.js
+++ HackerRank-Javascript/service-lane.js
@@ -1,15 +1,12 @@
-const serviceLane = (n, roadWidths, cases) => {
-  const result = [];
-
-  cases.forEach(possibleCase => {
-    let smallestWidth = roadWidths[possibleCase[0]];
-
-    for (let i = possibleCase[0]; i <= possibleCase[1]; i++) {
-      if (roadWidths[i] < smallestWidth) {
-        smallestWidth = roadWidths[i];
-      }
-    }
-    result.push(smallestWidth);
-  });
-  return result;
-};
+// Complete the serviceLane function below.
+function serviceLane(n, cases, width) {
+    let min = []
+    for(let i = 0; i<cases.length; i++){
+        let a = []
+        for(let x = cases[i][0]; x <= cases[i][1]; x++) {
+            a.push(width[x])
+        }
+        min.push(Math.min.apply(Math,a))
+    } 
+    return min
+}
